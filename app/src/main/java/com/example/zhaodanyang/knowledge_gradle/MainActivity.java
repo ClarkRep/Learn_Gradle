f8bc45a5fc4fb188c594e814dc339aad4c9f6bd5
zhaodanyang <zdy6542158>
2018-01-30 19:09:29 +0800

    first commit

---
+++ app/src/main/java/com/example/zhaodanyang/knowledge_gradle/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.zhaodanyang.knowledge_gradle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
