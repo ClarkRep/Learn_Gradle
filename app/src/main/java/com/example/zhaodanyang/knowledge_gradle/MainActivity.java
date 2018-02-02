477ed36bcc2864a8d1bb93aefd40ddc0d7871f49
zhaodanyang <zdy6542158>
2018-02-02 19:58:48 +0800

    提交图片

--- app/src/main/java/com/example/zhaodanyang/knowledge_gradle/MainActivity.java
+++ app/src/main/java/com/example/zhaodanyang/knowledge_gradle/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.zhaodanyang.knowledge_gradle;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiUrl = BuildConfig.API_URL;
+        Log.i("haha", apiUrl);
+
+        String string = getResources().getString(R.string.test);
+        Log.i("haha", string);
     }
 }
